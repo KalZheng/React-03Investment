d93c89fd0fcf023a755405e32c09a462b214d6d0
Kal Zheng <kal.zheng@inspire-dt.com>
2023-11-21 16:48:33 +0800

    add results components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Results from "./components/Results";
 
 function App() {
+  const [userInput, setUsetInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 10000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUsetInput((prevUserInput) => {
+      return { ...prevUserInput, [inputIdentifier]: newValue };
+    });
+  };
+
   return (
     <>
-      <Header></Header>
-      <UserInput></UserInput>
+      <Header/>
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
